9d3435b9f32fe7c71d0ddce5240a7fd6073c0590
gabr0 <gnogueral1@alumno.uned.es>
2018-10-23 12:45:58 +0200

    Propper write the symbols

--- Rombo.cpp
+++ Rombo.cpp
@@ -15,32 +15,39 @@ for(int fila=1 ; fila<=altura ; fila++){
     printf(" ");
     }
   for (int caracterporfila=fila ; caracterporfila>=1;caracterporfila--){
-    if(caracterporfila%3 == 1){printf("*");}
-    else if(caracterporfila%3 == 2){printf("@");}
-    else if(caracterporfila%3 == 0){printf("#");}
+    if(caracterporfila%4 == 2){printf("o");}
+    else if(caracterporfila%4 == 1){printf(".");}
+    else if(caracterporfila%4 == 0){printf("@");}
+    else if(caracterporfila%4 == 3){printf(".");}
     }
     for (int caracterporfila=fila-1 ; caracterporfila>=1;caracterporfila--){
-    if(caracterporfila%3 == 1){printf("*");}
-    else if(caracterporfila%3 == 2){printf("@");}
-    else if(caracterporfila%3 == 0){printf("#");}
+    if((fila-1-caracter)%4==0){printf("o");}
+    else if((fila-1-caracter)%4 == 1){printf(".");}
+    else if((fila-1-caracter)%4 == 0){printf("@");}
+    else if((fila-1-caracter)%4 == 3){printf(".");}
+    caracter++;
     }
   printf("\n");
+  caracter=0;
   }
   for(int fila=altura-1; fila>=1; fila--){
     for (int espacios=altura-fila; espacios>=1;espacios--){
     printf(" ");
     }
   for (int caracterporfila=fila ; caracterporfila>=1;caracterporfila--){
-    if(caracterporfila%3 == 1){printf("*");}
-    else if(caracterporfila%3 == 2){printf("@");}
-    else if(caracterporfila%3 == 0){printf("#");}
+    if(caracterporfila%4 == 2){printf("o");}
+    else if(caracterporfila%4 == 1){printf(".");}
+    else if(caracterporfila%4 == 0){printf("@");}
+    else if(caracterporfila%4 == 3){printf(".");}
     }
   for (int caracterporfila=fila-1 ; caracterporfila>=1;caracterporfila--){
-    if((fila-1-caracter)%3 == 1){printf("*");}
-    else if((fila-1-caracter)%3 == 2){printf("@");}
-    else if((fila-1-caracter)%3 == 0){printf("#");}
+    if((fila-1-caracter)%4==0){printf("o");}
+    else if((fila-1-caracter)%4 == 1){printf(".");}
+    else if((fila-1-caracter)%4 == 0){printf("@");}
+    else if((fila-1-caracter)%4 == 3){printf(".");}
+    caracter++;
     }
     printf("\n");
-
+    caracter=0;
   }
 }
